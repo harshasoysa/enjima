9007b7c132994ef6f912c5711f4420bdc18f160b
Harsha Vimukthi <vimukthi.soysa@gmail.com>
2021-01-21 14:27:37 +0530

    test

--- public/index.php
+++ public/index.php
@@ -1,27 +1,23 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
 
-$app = require __DIR__.'/../bootstrap/app.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+$a = microtime();
 
-$app->run();
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$b = microtime();
+$request = Request::createFromGlobals();
+$c = microtime();
+
+$name = $request->get('name', 'World');
+$d = microtime();
+
+$name = htmlspecialchars($_GET['name']);
+
+$e = microtime();
+
+$response = new Response($a . '<br>' . $b . '<br>' . $c . '<br>' . $d . '<br>' . $e . '<br>' . $name);
+
+$response->send();
