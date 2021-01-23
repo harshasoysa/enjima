5bb81286b53ea9fe86f65684127f7b1050e397c9
Harsha Vimukthi <vimukthi.soysa@gmail.com>
2021-01-23 23:13:02 +0530

    remove test

--- public/index.php
+++ public/index.php
@@ -2,22 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$a = microtime();
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$b = microtime();
 $request = Request::createFromGlobals();
-$c = microtime();
 
-$name = $request->get('name', 'World');
-$d = microtime();
+$name = $request->get('name');
 
-$name = htmlspecialchars($_GET['name']);
-
-$e = microtime();
-
-$response = new Response($a . '<br>' . $b . '<br>' . $c . '<br>' . $d . '<br>' . $e . '<br>' . $name);
+$response = new Response($name);
 
 $response->send();
