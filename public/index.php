3303c41fffe1aa20b9fb7f4fb73fc0465cfba8de
Harsha Vimukthi <vimukthi.soysa@gmail.com>
2021-01-27 13:24:13 +0530

    added httpkernel

--- public/index.php
+++ public/index.php
@@ -5,46 +5,35 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
 use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
 use Symfony\Component\HttpKernel;
 use Symfony\Component\Routing;
+use Symfony\Component\Routing\RouteCollection;
 
 $request = Request::createFromGlobals();
+$requestStack = new RequestStack();
 
-$routes = new Routing\RouteCollection();
+$routes = new RouteCollection();
 
 $routes->add('is_leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
     '_controller' => '\App\Http\Controllers\LeapYearController::index',
 ]));
 
-$a = microtime(true);
-
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
-
-// $compiledRoutes = (new CompiledUrlMatcherDumper($routes))->getCompiledRoutes();
-
-// $_SESSION["compiledRoutes"] = $compiledRoutes;
-
-// $compiledRoutes = $_SESSION["compiledRoutes"];
-// dd($compiledRoutes);
-
-// $matcher = new CompiledUrlMatcher($compiledRoutes, $context);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
 $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
 $dispatcher = new EventDispatcher();
 
-$framework = new Enjima\Core\Application($dispatcher, $matcher, $controllerResolver, $argumentResolver);
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
 
-$framework = new HttpKernel\HttpCache\HttpCache(
-    $framework,
-    new HttpKernel\HttpCache\Store(__DIR__.'/../cache')
-);
+$framework = new Enjima\Core\Application($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+
 
 $response = $framework->handle($request);
 
